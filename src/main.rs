889fb4a10830c0ebe66c7d882f18480c4879f77d
Emeka Allison <emekaallison4@gmail.com>
2023-04-28 00:00:36 +0100

    Feat: Game terminal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use crossterm::cursor::{Hide, Show};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{terminal, ExecutableCommand};
+use rusty_audio::Audio;
 use std::error::Error;
 
-use rusty_audio::Audio;
+use std::io;
+
 fn main() -> Result<(), Box<dyn Error>> {
+    // Set up audio
     let mut audio = Audio::new();
     audio.add("explode", "./sounds/explode.wav");
     audio.add("lose", "./sounds/lose.wav");
@@ -10,8 +16,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("start", "./sounds/start.wav");
     audio.add("win", "./sounds/win.wav");
 
+    // Play music at start
     audio.play("explode");
-    audio.wait();
 
+    // Game view Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Clean up
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
