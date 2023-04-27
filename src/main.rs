8f96c864cd51091931979cbf796b1e8d2ef00f53
Emeka Allison <emekaallison4@gmail.com>
2023-04-27 23:53:54 +0100

    Feat: Added sound effects

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use rusty_audio::Audio;
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "./sounds/explode.wav");
+    audio.add("lose", "./sounds/lose.wav");
+    audio.add("move", "./sounds/move.wav");
+    audio.add("phew", "./sounds/phew.wav");
+    audio.add("start", "./sounds/start.wav");
+    audio.add("win", "./sounds/win.wav");
+
+    audio.play("explode");
+    audio.wait();
+
+    Ok(())
 }
