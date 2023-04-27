04a1abfc9e02166212279de828dcec40af875bc1
Emeka Allison <emekaallison4@gmail.com>
2023-04-28 00:08:17 +0100

    Feat: game loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::{terminal, ExecutableCommand};
+use crossterm::{event, terminal, ExecutableCommand};
 use rusty_audio::Audio;
 use std::error::Error;
 
 use std::io;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Set up audio
@@ -17,13 +19,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("win", "./sounds/win.wav");
 
     // Play music at start
-    audio.play("explode");
+    audio.play("start");
 
     // Game view Terminal
     let mut stdout = io::stdout();
     terminal::enable_raw_mode()?;
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
+
+    // Game loop
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
 
     // Clean up
     audio.wait();
